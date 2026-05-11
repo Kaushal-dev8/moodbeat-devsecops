3944665866f3160556f2f8f9ba4a7385a9776744
Kaushal Vaidya <kaushal.vaidya24@vit.edu>
2026-05-11 12:13:54 +0530

    Build playlist API routes in FastAPI

--- backend/venv/app/main.py
+++ backend/venv/app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from app.routes.playlist import router as playlist_router
 
 app = FastAPI()
 
 
 @app.get("/")
 def root():
-    return {"message": "MoodBeat Backend Running 🚀"}
+    return {
+        "message": "MoodBeat Backend Running 🚀"
+    }
+
+
+app.include_router(playlist_router)
