cab58ad9fb19e55e2f64252b4a53ab5afd456589
Kaushal Vaidya <kaushal.vaidya24@vit.edu>
2026-05-19 00:06:18 +0530

    Setup full Dockerized MoodBeat stack with React FastAPI Nginx and Tailwind

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar"
-import MoodCard from "../components/Moodcard"
+import MoodCard from "../components/MoodCard";
 import { Link } from "react-router-dom"
 
 function Home() {
